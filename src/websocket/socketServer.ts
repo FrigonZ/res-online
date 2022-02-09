cab39e1264a0c03659c20864f2e75899270b71a6
larrylzhang <larrylzhang@tencent.com>
2022-02-09 18:52:46 +0800

    feat: ws与http服务共享端口

--- src/websocket/socketServer.ts
+++ src/websocket/socketServer.ts
@@ -2,9 +2,6 @@ import Websocket = require('ws');
 import { doLog } from '../utils/logger';
 import { SocketItem } from './socket';
 
-/** websocket服务监听端口 */
-const PORT = 443;
-
 /** 单例socket server */
 let wss: Websocket.Server;
 
@@ -17,10 +14,8 @@ const handleConnect = (socket: Websocket) => {
   doLog(`receive socket key=${socketItem.key}`);
 };
 
-/** 获取socket server单例 */
-export const getWsServer = () => {
-  if (wss) return wss;
-  wss = new Websocket.Server({ port: PORT });
+/** 初始化websocket server */
+export const initWsServer = (server: any) => {
+  wss = new Websocket.Server({ server });
   wss.on('connection', handleConnect);
-  return wss;
 };
