d05a5203bdb1d98aff378d4a9c56809a34516637
larrylzhang <larrylzhang@tencent.com>
2022-02-21 16:03:44 +0800

    fix: 修复中间件异步问题

--- src/middleware/interrupt.ts
+++ src/middleware/interrupt.ts
@@ -1,11 +1,11 @@
 import { Ctx, Next } from '../constant';
 import { ResponseWrap } from '../utils/response';
 
-export const removeIllegal = (ctx: Ctx, next: Next) => {
+export const removeIllegal = async (ctx: Ctx, next: Next) => {
   const { user, admin } = ctx.request.body || {};
   if (user || admin) {
     ResponseWrap.fail(ctx, '参数不合法');
     return;
   }
-  next();
+  await next();
 };
